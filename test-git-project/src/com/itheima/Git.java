44a67ebe2ab8eed2d51cdc9b28f159c620e08e60
itheima <505105958@qq.com>
2019-03-29 17:27:59 +0800

    first,class

--- test-git-project/src/com/itheima/Git.java
+++ test-git-project/src/com/itheima/Git.java
@@ -5,7 +5,7 @@ public class Git {
         System.out.println("hello,git");
         System.out.println("hello,git");
         System.out.println("hello,git");
-        System.out.println(1);
+
 
     }
 
